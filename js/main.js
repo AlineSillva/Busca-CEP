709eadc10a55da4d6af2f033ef408ed539eedf1f
Aline Evangelista <111699963+AlineSillva@users.noreply.github.com>
2022-10-21 08:10:42 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function consultaCEP () {
+    var cep = document.getElementById("cep").value;
+    var url = `https://viacep.com.br/ws/${cep}/json/`;
+    console.log(url)
+    $.ajax ({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
+        }
+    })
+}
